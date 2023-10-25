7f4095deb1d22f3263a4666a91daac89e783b96c
Kye-Working <kyefworkin@gmail.com>
2023-10-25 19:10:12 +0100

    Intro
    - Added Intro
    (Basic JS variables)

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import Intro from "./Intro.jsx";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Intro></Intro>
       <Footer></Footer>
     </>
   );
---
+++ my-project/src/Intro.jsx
@@ -0,0 +1,11 @@
+function Intro() {
+  const name = "Kye";
+  return (
+    <div className="bg-orange-600">
+      <p>- Hi, my name is {name.toUpperCase()} -</p>
+      <hr />
+    </div>
+  );
+}
+
+export default Intro;
